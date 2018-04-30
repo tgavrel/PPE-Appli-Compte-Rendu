bdbb685c76def598cdd1596a94aa7ac0318550d0
tgavrel <tomas.gavrel@hotmail.fr>
2018-04-30 02:35:50 +0200

    First commit

---
+++ Appli/inc/connexion.php
@@ -0,0 +1,9 @@
+<?php
+
+$databaseHost = '127.0.0.1';
+$databaseName = 'gsb_compte-rendu';
+$databaseUsername = 'root';
+$databasePassword = 'rootroot';
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+	
+?>
